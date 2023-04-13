8697d3863ac739ea3b7a59c08cafcb27df9e560a
poojakaneriya <kaneriyap22@gmail.com>
2023-04-13 12:10:17 +0530

    commit

---
+++ app/src/main/java/com/example/texoapp/Page19.java
@@ -0,0 +1,14 @@
+package com.example.texoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page19 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page19);
+    }
+}
